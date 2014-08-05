3522a06dc9f9ddf398748caa0033661cc63808e2
amyvs <amydecat@yahoo.com>
2014-08-04 18:12:11 -0700

    make Contact.all array global

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,12 +1,14 @@
 class Contact
   attr_reader :name, :email
 
+  @@contacts = []
+
   def initialize(name)
     @name = name
     @email = []
   end
 
   def Contact.all
-    []
+    @@contacts
   end
 end
