5419e7c93947374416c5c1093516ddd029f04a2c
amyvs <amydecat@yahoo.com>
2014-08-04 22:31:19 -0700

    add menu for editing contacts

--- email_keeper.rb
+++ email_keeper.rb
@@ -4,7 +4,7 @@ require './lib/email'
 def main_menu
   puts "Welcome to your Email Keeper. Please select from the following:"
   puts "Press 'a' to add a new contact, 'l' to list all of your contacts,"
-  puts "or 'x' to exit."
+  puts "'e' to edit a contact, or 'x' to exit."
 
   user_choice = gets.chomp
 
@@ -12,6 +12,8 @@ def main_menu
     add_contact
   elsif user_choice == 'l'
     list_contacts
+  elsif user_choice == 'e'
+    edit_contact
   elsif user_choice == 'x'
     puts "Have a good one!"
     exit
@@ -40,7 +42,28 @@ end
 def list_contacts
   puts "Here's a list of all of your contacts:"
   Contact.all.each_with_index { |contact, index| puts "#{index+1}. #{contact.name}" }
-  main_menu
+  puts "\n"
+end
+
+def edit_contact
+  puts "Please enter your contact's name."
+
+  contact_name = gets.chomp
+  @selected_contact = Contact.all.select { |contact| contact.name == contact_name }.first
+  puts "You've selected #{@selected_contact.name}\n"
+  puts "Press 'a' to add a new email address, 'r' to remove an email address,"
+  puts "or any key to return to the main menu."
+
+  user_choice = gets.chomp
+
+  if user_choice == 'a'
+    add_email
+  elsif user_choice == 'r'
+    remove_email
+  else
+    puts "Returning to the main menu..."
+    main_menu
+  end
 end
 
 main_menu
