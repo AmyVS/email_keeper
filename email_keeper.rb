4b6c79ea0902b46f6d3b66502833dcaefb018046
amyvs <amydecat@yahoo.com>
2014-08-04 22:48:08 -0700

    add menu for editing contacts

--- email_keeper.rb
+++ email_keeper.rb
@@ -1,6 +1,8 @@
 require './lib/contact'
 require './lib/email'
 
+@selected_contact = nil
+
 def main_menu
   puts "Welcome to your Email Keeper. Please select from the following:"
   puts "Press 'a' to add a new contact, 'l' to list all of your contacts,"
@@ -43,6 +45,7 @@ def list_contacts
   puts "Here's a list of all of your contacts:"
   Contact.all.each_with_index { |contact, index| puts "#{index+1}. #{contact.name}" }
   puts "\n"
+
   main_menu
 end
 
@@ -52,19 +55,33 @@ def edit_contact
   contact_name = gets.chomp
   @selected_contact = Contact.all.select { |contact| contact.name == contact_name }.first
   puts "You've selected #{@selected_contact.name}\n"
-  puts "Press 'a' to add a new email address, 'r' to remove an email address,"
-  puts "or any key to return to the main menu."
+  puts "Press 'a' to add a new email address, 'l to list a specific contact's email addresses,"
+  puts "'r' to remove an email address, or any key to return to the main menu."
 
   user_choice = gets.chomp
 
   if user_choice == 'a'
-    add_email
+    new_email
+  elsif user_choice == 'l'
+    list_emails
   elsif user_choice == 'r'
     remove_email
   else
     puts "Returning to the main menu..."
     main_menu
   end
+end
+
+def new_email
+  puts "Please enter the new email address for your contact, #{@selected_contact.name}."
+
+  user_input = gets.chomp
+  new_address = Email.new(user_input)
+  @selected_contact.add_email(new_address)
+
+  puts "You've successfully added #{new_address.address} to #{@selected_contact.name}'s email list.\n\n"
+  puts "Returning to the main menu. \n\n"
+  main_menu
 end
 
 main_menu
