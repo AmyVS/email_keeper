ab9109ba49d22c88679797b7d47c6b5f89291895
amyvs <amydecat@yahoo.com>
2014-08-04 18:00:46 -0700

    add a method for reading contact name out

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,4 +1,6 @@
 class Contact
+  attr_reader :name
+
   def initialize(name)
     @name = name
   end
--- spec/email_keeper_spec.rb
+++ spec/email_keeper_spec.rb
@@ -6,4 +6,9 @@ describe Contact do
     new_contact = Contact.new('The Doctor')
     expect(new_contact).to be_an_instance_of Contact
   end
+
+  it 'reads the contact name out' do
+    new_contact = Contact.new('The Doctor')
+    expect(new_contact.name).to eq 'The Doctor'
+  end
 end
