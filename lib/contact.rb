b6f58f794fc364884e7a4e5d2fac74ad2ca8061f
amyvs <amydecat@yahoo.com>
2014-08-07 22:12:27 -0700

    add a method to list all emails for a specific contact

--- lib/contact.rb
+++ lib/contact.rb
@@ -23,4 +23,10 @@ class Contact
   def remove_email(address)
     @emails.delete_if { |email| email == address }
   end
+
+  def list_emails
+    @emails.each_with_index do |email, index|
+      puts "#{index+1}. #{email.address}"
+    end
+  end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -44,6 +44,15 @@ describe Contact do
     test_contact.add_email(test_email)
     expect(test_contact.remove_email(test_email)).to eq []
   end
+
+  it 'lists all emails attached to a specified contact' do
+    test_contact = Contact.new('The Doctor')
+    test_email1 = Email.new('bluebox@tardis.com')
+    test_email2 = Email.new('wibblywobbly@timeywimey.com')
+    test_contact.add_email(test_email1)
+    test_contact.add_email(test_email2)
+    expect(test_contact.list_emails).to eq [test_email1, test_email2]
+  end
 end
 
 
